63f59a08cf5ef4920ec58b22681068cb19b35f4d
tradeview113d8-code <tradeview113d8@gmail.com>
2026-06-03 13:40:34 +0700

    Update auth.py

--- Telegram/auth.py
+++ Telegram/auth.py
@@ -1,4 +1,9 @@
-from Core.config import ADMIN_IDS
+import os
 
-def is_admin(telegram_id):
-    return telegram_id in ADMIN_IDS
+def is_admin(user_id):
+    # Lấy ID từ cấu hình Render
+    admin_id_env = os.environ.get("ADMIN_ID", "")
+    
+    # Ép kiểu cả 2 về chuỗi văn bản (String) để so sánh tuyệt đối
+    return str(user_id).strip() == str(admin_id_env).strip()
+    
