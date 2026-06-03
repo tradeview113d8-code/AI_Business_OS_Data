c3b05b9ab303fbca0af8f81da382f40474a5fc2f
tradeview113d8-code <tradeview113d8@gmail.com>
2026-06-03 13:50:52 +0700

    Update auth.py

--- Telegram/auth.py
+++ Telegram/auth.py
@@ -1,9 +1,6 @@
 import os
 
 def is_admin(user_id):
-    # Lấy ID từ cấu hình Render
     admin_id_env = os.environ.get("ADMIN_ID", "")
-    
-    # Ép kiểu cả 2 về chuỗi văn bản (String) để so sánh tuyệt đối
     return str(user_id).strip() == str(admin_id_env).strip()
     
